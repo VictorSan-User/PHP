1e2801b81253ef5fcab0822134e4481eae355177
Victor Nacife <167594971+VictorSan-User@users.noreply.github.com>
2024-08-10 10:42:31 -0300

    Atribuições
    Atribuições em PHP são basicamente atribuições feitas em Lógica de programação

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desconto</title>
+</head>
+<body>
+    <?php
+    $a = 3;
+    $b = $a;
+    $b +=17;
+
+    echo "Valor de a: $a<br/>Valor de b: $b";
+    ?>
+</body>
+</html>
