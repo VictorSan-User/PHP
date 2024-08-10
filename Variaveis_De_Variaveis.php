afc9e67599a490d799938a3736cf1ea761e92702
Victor Nacife <167594971+VictorSan-User@users.noreply.github.com>
2024-08-10 10:54:10 -0300

    Variáveis de Variáveis
    Pouco utilizadas, porém existem!

---
+++ Variaveis_De_Variaveis.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desconto</title>
+</head>
+<body>
+    <?php
+    $x="abc";
+    $$x="def";
+
+    echo "O conteudo da variavel x: $x";
+    echo "<br/>O conteudo da variavel criada recebeu: $abc";
+    ?>
+</body>
+</html>
